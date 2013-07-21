d1becb46616fa4bfa789375d959496d4538e6e37
François Févotte <fevotte@gmail.com>
2013-07-22 00:19:53 +0200

    Code comments

--- libclang++/index.hxx
+++ libclang++/index.hxx
@@ -6,16 +6,45 @@
 #include <string>
 
 namespace LibClang {
+  // Forward declaration
   class TranslationUnit;
 
+
+  /** @brief A set of translation units
+   *
+   * This is a proxy for libclang's @c CXIndex type
+   */
   class Index {
   public:
+    /** @brief default constructor
+     */
     Index ();
+
+    /** @brief Create a translation unit from a command-line
+     *
+     * Return the translation unit for a given source file and the provided
+     * command-line arguments which would be passed to the compiler.
+     *
+     * @param argc  The number of command-line arguments
+     * @param argv  A C-style array of command-line arguments
+     *
+     * @return The corresponfing TranslationUnit object
+     */
     TranslationUnit parse (int argc, char const *const *const argv) const;
+
+    /** @brief Create a translation unit from a command-line
+     *
+     * Return the translation unit for a given source file and the provided
+     * command-line arguments which would be passed to the compiler.
+     *
+     * @param args  A vector of command-line arguments
+     *
+     * @return The corresponfing TranslationUnit object
+     */
     TranslationUnit parse (const std::vector<std::string> & args) const;
-    const CXIndex & raw() const;
 
   private:
+    const CXIndex & raw() const;
     struct Index_ {
       CXIndex index_;
       Index_ (CXIndex index) : index_ (index) {}
--- libclang++/translationUnit.hxx
+++ libclang++/translationUnit.hxx
@@ -6,23 +6,91 @@
 #include "unsavedFiles.hxx"
 
 namespace LibClang {
+  // Forward declarations
   class Index;
   class SourceLocation;
   class Cursor;
 
+  /** @brief A translation unit
+   *
+   * This class is a proxy for libclang's \c CXTranslationUnit type and should
+   * provide the same features, except its lifetime should not have to be
+   * explicitly managed.
+   *
+   * The preferred way to create TranslationUnit objects is calling Index::parse.
+   */
   class TranslationUnit {
   public:
+    /** @brief Constructor from a \c CXTranslationUnit object
+     *
+     * This constructor converts a libclang @c CXTranslationUnit into a
+     * libclang++ TranslationUnit object.
+     *
+     * @param tu  A @c CXTranslationUnit object
+     */
     TranslationUnit (CXTranslationUnit tu);
 
+    /** @brief Reparse the translation unit
+     *
+     * Re-parse the source files used to create the translation unit, reading
+     * up-to-date source code from the file-system. The source code is re-parsed
+     * with the same command-line options than the first time.
+     */
     void reparse ();
+
+    /** @brief Reparse the translation unit
+     *
+     * Re-parse the source files used to create the translation unit, reading
+     * up-to-date content from the set of in-memory buffers in @em unsaved. The
+     * source code is re-parsed with the same command-line options than the
+     * first time.
+     *
+     * @param unsaved  A set of unsaved contents for the source files
+     */
     void reparse (UnsavedFiles & unsaved);
 
+    /** @brief Get the source location for a file/offset in the translation unit
+     *
+     * Retrieve the source location associated to a given source file and
+     * character offset in the translation unit.
+     *
+     * @param fileName  The source file name
+     * @param offset    The offset, counted in characters from the file beginning
+     *
+     * @return A SourceLocation object for the relevant location.
+     */
     SourceLocation getLocation (const char* fileName, unsigned int offset);
+
+    /** @brief Get a cursor to the translation unit
+     *
+     * This cursor can be used to start traversing the Abstract Syntax Tree
+     * (AST) of the translation unit.
+     *
+     * @return A Cursor representing the whole translation unit
+     */
     Cursor cursor () const;
 
+    /** @brief Get the number of diagnostic messages for the translation unit
+     *
+     * These diagnostic messages are those emitted by the compiler when parsing
+     * the translation unit.
+     *
+     * @return The number of diagnostic messages
+     */
     unsigned int numDiagnostics ();
+
+    /** @brief Get the i-th diagnostic message for the translation unit
+     *
+     * These diagnostic messages are those emitted by the compiler when parsing
+     * the translation unit.
+     *
+     * @param i  index of the diagnostic message (must be less than numDiagnostics())
+     *
+     * @return A string representing the message
+     */
     std::string diagnostic (unsigned int i);
 
+    // TODO Make this method private
     const CXTranslationUnit & raw () const;
 
   private:
@@ -32,5 +100,7 @@ namespace LibClang {
       ~TranslationUnit_ () { clang_disposeTranslationUnit (translationUnit_); }
     };
     std::shared_ptr<TranslationUnit_> translationUnit_;
+
+    friend class Cursor;
   };
 }
