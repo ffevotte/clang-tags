2e740e3f497bee560c41142af2335f45517887c5
François Févotte <fevotte@gmail.com>
2013-07-25 16:35:53 +0200

    Generation of the user documentation with test cases results

--- tests/src/main.cxx
+++ tests/src/main.cxx
@@ -2,33 +2,33 @@
 #include <iostream>
 
 #ifdef DEBUG
-inline void debug (const char* message) {
+inline void debug (const char* message) {                //(ref:debug1)
   std::cerr << message << std::endl;
 }
 #else
-inline void debug (const char* message) {}
+inline void debug (const char* message) {}               //(ref:debug2)
 #endif
 
 template <typename T> struct MyClass {
-  MyClass () { debug ("MyClass::MyClass"); }
+  MyClass () { debug ("MyClass::MyClass"); }             //(ref:defCtor)
 
-  void display () {
+  void display () {                                      //(ref:defDisplayT)
     std::cout << "MyClass<T>::display()" << std::endl;
   }
 };
 
 template <> struct MyClass<int> {
-  void display () {
+  void display () {                                      //(ref:defDisplayInt)
     std::cout << "MyClass<int>::display()" << std::endl;
   }
 };
 
 int main () {
-  int display = 3;
+  int display = 3;                                       //(ref:display1)
 
-  MyClass<double> a;
-  a.display();
+  MyClass<double> a;                                     //(ref:ctor)
+  a.display();                                           //(ref:display2)
 
   MyClass<int> b;
-  b.display();
+  b.display();                                           //(ref:display3)
 }
